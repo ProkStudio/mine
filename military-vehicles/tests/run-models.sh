d6f4386f0783c0e14aa4e4b1eaa38bcf59ce0ae0
Michael <162596416+ProkStudio@users.noreply.github.com>
2026-09-09 16:56:31 +0300

    Military Vehicles alpha.8: проверки подвески и оснастки колёсных машин

--- military-vehicles/tests/run-models.sh
+++ military-vehicles/tests/run-models.sh
@@ -6,9 +6,13 @@ javac --release 21 -d build/fleet-models \
   src/main/java/com/prokstudio/militaryvehicles/core/TruckGeometry.java \
   src/main/java/com/prokstudio/militaryvehicles/core/VehicleKind.java \
   src/main/java/com/prokstudio/militaryvehicles/core/ExpansionGeometry.java \
+  src/main/java/com/prokstudio/militaryvehicles/core/VehicleGeometry.java \
   src/test/java/com/prokstudio/militaryvehicles/FleetModelCases.java \
   src/test/java/com/prokstudio/militaryvehicles/FleetModelSmoke.java \
   src/test/java/com/prokstudio/militaryvehicles/ServiceModelCases.java \
-  src/test/java/com/prokstudio/militaryvehicles/ServiceModelSmoke.java
+  src/test/java/com/prokstudio/militaryvehicles/ServiceModelSmoke.java \
+  src/test/java/com/prokstudio/militaryvehicles/WheeledModelCases.java \
+  src/test/java/com/prokstudio/militaryvehicles/WheeledModelSmoke.java
 java -cp build/fleet-models com.prokstudio.militaryvehicles.FleetModelSmoke
 java -cp build/fleet-models com.prokstudio.militaryvehicles.ServiceModelSmoke
+java -cp build/fleet-models com.prokstudio.militaryvehicles.WheeledModelSmoke
